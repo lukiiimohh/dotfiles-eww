5b7b72c202845b9fef572c61f3912307f49ad770
lukiiimohh <118058029+lukiiimohh@users.noreply.github.com>
2022-12-12 20:09:45 +0000

    Add files via upload

---
+++ eww/scripts/copyiptarget.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+target=$(cat ~/.config/eww/scripts/target)
+echo -ne $target | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+
+notify-send "SYSTEM" "IP TARGET copiada en el portapapeles"
